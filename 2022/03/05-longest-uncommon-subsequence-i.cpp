37ed8a558112ce85f20522bc9548be46843842a9
Xhofe <927625802@qq.com>
2022-03-08 21:07:08 +0800

    2022-03-05

---
+++ 2022/03/05-longest-uncommon-subsequence-i.cpp
@@ -0,0 +1,11 @@
+// https://leetcode-cn.com/problems/longest-uncommon-subsequence-i/
+
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        if(a==b){
+            return -1;
+        }
+        return max(a.size(),b.size());
+    }
+};
